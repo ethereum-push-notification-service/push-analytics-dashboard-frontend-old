edce330ec400c4722c6d81f1d602c1a2e5a54602
thea-exe <csokkunthea@gmail.com>
2022-07-14 14:18:10 -0400

    API_URL change

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.analytics.epns.io/api/analytics';
+const API_URL = 'https://api.analytics.epns.io/apis/analytics';
 
 export const getTotalNumberOfChannels = async () => {
   try {
-    const response = await axios.post(`${API_URL}/get_total_channel_count`, {});
+    const response = await axios.post(`${API_URL}/get_total_channel_count_weekly`, {});
 
     return response.data;
   } catch (error) {
