61338a2de9cf56678fa6c66485e659be67a357e9
Arpitha Rajeev <105787510+Arpitha-Rajeev@users.noreply.github.com>
2022-08-12 16:01:40 +0530

    Update Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,32 +1,38 @@
 import { useEffect, useState } from 'react';
-import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography, Box, Card, CardContent, Stack } from '@mui/material';
+import { Grid, Container, Typography, Box, Card, CardContent, Stack, CircularProgress } from '@mui/material';
 import Layout from 'components/layout';
 import {
   getTotalNumberOfChannels,
   getTotalNumberOfSubscribers,
   getTotalNotifications,
-  getNotificationsPerWeek,
+  pushTradingVolume,
+  topChannels,
 } from 'utils/api';
-import { AppCurrentVisits, AppWebsiteVisits, AppTrafficBySite, AppWidgetSummary } from './components';
+import { AppWidgetSummary } from './components';
 
 import SubscribersWeeklyCount from './components/SubscribersWeeklyCount';
 import ChannelsPerWeek from './components/ChannelsPerWeek';
+import NotificationsWeeklyCount from './components/NotificationsWeeklyCount';
+import Compare from './components/Compare';
+import TopChannelsView from './components/TopChannelsView';
 
 const Home = () => {
-  const theme = useTheme();
 
   const [channelsCount, setChannelsCount] = useState(0);
   const [subscriberCount, setSubscriberCount] = useState(0);
   const [totalNotifications, setTotalNotifications] = useState(0);
+  const [pushTrading, setPushTrading] = useState(0);
+  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       setChannelsCount(await getTotalNumberOfChannels());
       setSubscriberCount(await getTotalNumberOfSubscribers());
       setTotalNotifications(await getTotalNotifications());
-
-      console.log(await getNotificationsPerWeek());
+      setPushTrading(await pushTradingVolume());
+      setName((await topChannels()).channels)
+      setLoading(false);
     })();
   }, []);
 
@@ -42,7 +48,8 @@ const Home = () => {
             <AppWidgetSummary
               title="Total Notifications"
               total={totalNotifications}
-              icon={'ant-design:user-outlined'}
+              color="warning"
+              icon={'ant-design:bell-filled'}
             />
           </Grid>
 
@@ -50,8 +57,7 @@ const Home = () => {
             <AppWidgetSummary
               title="Number of Subscriptions"
               total={subscriberCount}
-              color="info"
-              icon={'ant-design:wechat-outlined'}
+              icon={'ant-design:user-outlined'}
             />
           </Grid>
 
@@ -59,31 +65,77 @@ const Home = () => {
             <AppWidgetSummary
               title="Number of Channels"
               total={channelsCount}
-              color="warning"
-              icon={'ant-design:bell-filled'}
+              color="info"
+              icon={'ant-design:wechat-outlined'}
             />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="$PUSH Trading Volume" total={0} color="error" icon={'ant-design:bug-filled'} />
+            <AppWidgetSummary title="$PUSH Trading Volume" total={pushTrading} color="error" icon={'ant-design:bug-filled'} />
           </Grid>
 
+          {/* <TopChannelsView /> */}
           <Grid item xs={12} md={6} lg={12}>
             <Card>
-              <CardContent>
-                <Stack direction="row" spacing={7}>
-                  <Typography variant="h5" sx={{ mt: 4, mr: 4 }}>
-                    Top Channels on EPNS
+            <Typography variant="h4" sx={{ mt: 4, mr: 4, ml: 4, mb: 4 }}>
+                   <center> Top Channels on EPNS</center>
                   </Typography>
-                  <Box component="img" src="/static/aave-logo.svg" sx={{ width: 150, height: 100 }} />
-                  <Box component="img" src="/static/maker-logo.svg" sx={{ width: 150, height: 100 }} />
-                  <Box component="img" src="/static/ens-logo.svg" sx={{ width: 150, height: 100 }} />
+              <CardContent>
+                <Stack direction="row" spacing={20}>
+                  
+                  {loading ? (
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        width: '100%',
+                        textAlign: 'center',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <CircularProgress size={50} />
+                    </Box>
+                  ) : (
+                    <>
+                      <Box component="img" src={name[0].icon} sx={{ width: 150, height: 100 }} />
+                      <Box component="img" src={name[1].icon} sx={{ width: 150, height: 100 }} />
+                      <Box component="img" src={name[2].icon} sx={{ width: 150, height: 100 }} />
+                      <Box component="img" src={name[3].icon} sx={{ width: 150, height: 100 }} />
+                    </>
+                  )}
                 </Stack>
               </CardContent>
+              <CardContent>
+                <Stack direction="row" spacing={20}>
+                  
+                  {loading ? (
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        width: '100%',
+                        textAlign: 'center',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <CircularProgress size={50} />
+                    </Box>
+                  ) : (
+                    <>
+                      <Box component="img" src={name[5].icon} sx={{ width: 150, height: 100 }} />
+                      <Box component="img" src={name[6].icon} sx={{ width: 150, height: 100 }} />
+                      <Box component="img" src={name[7].icon} sx={{ width: 150, height: 100 }} />
+                      <Box component="img" src={name[8].icon} sx={{ width: 150, height: 100 }} />
+                    </>
+                  )}
+                </Stack>
+              </CardContent>
+
             </Card>
           </Grid>
 
-          <Grid item xs={12} md={6} lg={8}>
+          {/* <TopChannelsView /> */}
+          {/* <Grid item xs={12} md={6} lg={8}>
             <AppWebsiteVisits
               title="$PUSH compared to ETH and Matic"
               subheader="(+43%) than last year"
@@ -121,9 +173,11 @@ const Home = () => {
                 },
               ]}
             />
-          </Grid>
+          </Grid> */}
 
-          <Grid item xs={12} md={6} lg={4}>
+
+
+          {/* <Grid item xs={12} md={6} lg={4}>
             <AppCurrentVisits
               title="Total Users by Platform"
               chartData={[
@@ -139,7 +193,7 @@ const Home = () => {
                 theme.palette.chart.red[1],
               ]}
             />
-          </Grid>
+          </Grid> */}
         </Grid>
 
         <Typography variant="h5" sx={{ my: 5 }}>
@@ -151,9 +205,15 @@ const Home = () => {
 
           <ChannelsPerWeek />
 
-          <Grid item xs={12} md={6} lg={6}>
+          <NotificationsWeeklyCount />
+
+          <Compare />
+
+          {/* <Grid item xs={12} md={6} lg={6}>
             <AppWebsiteVisits
-              title="Total Notifications per day"
+              title={`Notification count:  ${dayjs(dates[dates.length - 1]).format('ddd, MMMM D')} - ${dayjs(
+                dates[0]
+              ).format('ddd, MMMM D')}`}
               chartLabels={['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']}
               chartData={[
                 {
@@ -164,9 +224,9 @@ const Home = () => {
                 },
               ]}
             />
-          </Grid>
+          </Grid> */}
 
-          <Grid item xs={12} md={6} lg={6}>
+          {/* <Grid item xs={12} md={6} lg={6}>
             <AppTrafficBySite
               title="Governance"
               list={[
@@ -188,10 +248,10 @@ const Home = () => {
                 },
               ]}
             />
-          </Grid>
+          </Grid> */}
         </Grid>
 
-        <Typography variant="h5" sx={{ my: 5 }}>
+        {/* <Typography variant="h5" sx={{ my: 5 }}>
           Generating value for dApps, developers and users
         </Typography>
 
@@ -223,7 +283,7 @@ const Home = () => {
               </CardContent>
             </Card>
           </Grid>
-        </Grid>
+        </Grid> */}
       </Container>
     </Layout>
   );
